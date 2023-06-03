28c94f16448e7115eb2f07e83f872f1d0ff930aa
Higor Gabriel Lino Silva <65170741+H1gor1@users.noreply.github.com>
2023-06-03 10:10:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    document.getElementById("question").style.display = "none";
+    document.getElementById("buttons").style.display = "none";
+    document.getElementById("image-container").style.display = "block";
+    document.getElementById("image").src = "imagem_sim.png";
+  });
+  
+  document.getElementById("noButton").addEventListener("click", function() {
+    document.getElementById("question").style.display = "none";
+    document.getElementById("buttons").style.display = "none";
+    document.getElementById("image-container").style.display = "block";
+    document.getElementById("image").src = "imagem_nao.png";
+  });
