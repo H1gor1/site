0d176156c2488c640e9c0f8192b54515f0b06854
Higor Gabriel Lino Silva <65170741+H1gor1@users.noreply.github.com>
2023-07-12 17:11:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.getElementById("yesButton").addEventListener("click", function() {
-    document.getElementById("question").style.display = "none";
-    document.getElementById("buttons").style.display = "none";
-    document.getElementById("image-container").style.display = "block";
-    document.getElementById("image").src = "imagem_sim.png";
-  });
-  
-  document.getElementById("noButton").addEventListener("click", function() {
-    document.getElementById("question").style.display = "none";
-    document.getElementById("buttons").style.display = "none";
-    document.getElementById("image-container").style.display = "block";
-    document.getElementById("image").src = "imagem_nao.png";
-  });
+  document.getElementById("question").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("image-container").style.display = "block";
+  document.getElementById("image").src = "imagem_sim.png";
+  document.getElementById("buttons2").style.display = "block";
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+  document.getElementById("question").style.display = "none";
+  document.getElementById("buttons").style.display = "none";
+  document.getElementById("image-container").style.display = "block";
+  document.getElementById("image").src = "imagem_nao.png";
+});
